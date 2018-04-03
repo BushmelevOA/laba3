ff7d72694eef00cbb1aba70da376e0fb84a08a30
u211-06 <u211-06@public.mpei.local>
2018-04-03 12:07:53 +0300

    lalala

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <cassert>
+#include <vector>
 using namespace std;
 char Scale[] = "CKF";
 struct Temperature{
@@ -32,26 +33,122 @@ void test_temperature_input()
     assert (Temp.temp == -400);
     assert (Temp.scale == 'F');
 }
- double convert (const Temperature & Temp,char scale_to)
- {
+ double convert (const Temperature & Temp,char scale_to) {
      double Kelvins;
-     switch (Temperature.scale)
-     {
+     switch (Temp.scale) {
+     case 'k':
      case 'K': {
-           Kelvins = ((9.0/5)(-273)+32);
+         Kelvins = Temp.scale;
          break;
      }
-     case 'K1'
-
-
-
-
-
-
+     case 'f': {
+         Kelvins = ((9.0 / 5) * (Temp.temp - 273) + 32);
+         break;
+     }
+     case 'F': {
+         Kelvins = Temp.temp - 273.15;
+         break;
+     }
+     }
+     scale_to = Temp.scale;
+     switch (scale_to) {
+     case 'C':
+     case 'c': {
+         return Kelvins - 273.15;
+         break;
+     }
+     case 'K':
+     case 'k':
+         return Kelvins;
+         break;
      }
  }
+bool operator < (const Temperature& lhs, const Temperature& rhs){
+         return convert(lhs,'K')< convert(rhs, 'K');
+ }
+Temperature operator - (const Temperature& lhs,const Temperature& rhs){
+    Temperature ret_temp;
+    ret_temp.temp=convert (lhs, 'K') - convert (rhs, 'K');
+    ret_temp.scale = 'K';
+    return ret_temp;
+}
+Temperature operator / (const Temperature& lhs, const Temperature& rhs){
+    Temperature ret_temp;
+    ret_temp.temp=convert (lhs, 'K') / convert (rhs, 'K');
+    ret_temp.scale = 'K';
+    return ret_temp;
+}
+
 int main()
 {
-        test_temperature_input();
+    size_t number_count;
+       cerr << "Enter number count: ";
+       cin >> number_count;
+
+               cerr << "Enter numbers: ";
+       vector<Temperature> numbers(number_count);
+        for (size_t i = 0; i < number_count; i++) {
+                cin >> numbers[i];
+           }
+
+              size_t column_count;
+      cerr << "Enter column count: ";
+      cin >> column_count;
+
+               double min = numbers[0];
+    double max = numbers[0];
+       for (double number : numbers) {
+              if (number < min) {
+            min = number;
+        }
+             if (number > max) {
+                       max = number;
+                    }
+           }
+            vector<size_t> counts(column_count);
+       for (double number : numbers) {
+        size_t column = (size_t)((number - min) / (max - min) * column_count);
+               if (column == column_count) {
+                      column--;
+                 }
+               counts[column]++;
+           }
+
+               const size_t screen_width = 80;
+      const size_t axis_width = 4;
+       const size_t chart_width = screen_width - axis_width;
+
+                // Можно было бы считать в предыдущем цикле, но так более наглядно.
+                size_t max_count = 0;
+    for (size_t count : counts) {
+               if (count > max_count) {
+            max_count = count;
+        }
+            }const bool scaling_needed = max_count > chart_width;
+
+                for (size_t count : counts) {
+                if (count < 100) {
+                       cout << ' ';
+                   }
+               if (count < 10) {
+                      cout << ' ';
+                   }
+             cout << count << "|";
+
+                64   +        size_t height = count;
+              if (scaling_needed) {
+                        // Point: код должен быть в первую очередь понятным.
+                                const double scaling_factor = (double)chart_width / max_count;
+                       height = (size_t)(count * scaling_factor);
+                   }
+
+                       for (size_t i = 0; i < height; i++) {
+                        cout << '*';
+                   }
+                cout << '\n';   }
+
+                return 0;
+
+    test_temperature_input();
 }
 
